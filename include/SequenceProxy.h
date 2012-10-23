c528daa6a7bfa508931b7e9ef5674d1f06539ce6
Daniel Exner <dex@dragonslave.de>
2012-10-23 23:51:28 +0200

    * SequenceProxy belongs in namespace Circal

--- include/SequenceProxy.h
+++ include/SequenceProxy.h
@@ -22,118 +22,121 @@
 
 #include <Bpp/Seq/Sequence.h>
 
-/*
- *
- */
-class SequenceProxy: public bpp::BasicSequence
+namespace Circal
   {
-public:
-  SequenceProxy(const std::string& name, const std::vector<int>& sequence,
-      const bpp::Alphabet* alpha) throw (bpp::BadIntException);
-  SequenceProxy(const bpp::Sequence& sequence);
-  SequenceProxy(const std::string& name, const std::string& sequence, const bpp::Alphabet* alpha);
+    class SequenceProxy: public bpp::BasicSequence
+      {
+    public:
+      SequenceProxy(const std::string& name, const std::vector<int>& sequence,
+          const bpp::Alphabet* alpha) throw (bpp::BadIntException);
+      SequenceProxy(const bpp::Sequence& sequence);
+      SequenceProxy(const std::string& name, const std::string& sequence,
+          const bpp::Alphabet* alpha);
 
-  virtual ~SequenceProxy();
+      virtual ~SequenceProxy();
 
-  void shuffle()
-    {
-      bpp::BasicSequence::shuffle();
-    }
-  ;
-  void deleteElement(unsigned int pos) throw (bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::deleteElement(pos);
-    }
-  ;
-  void deleteElements(unsigned int pos, unsigned int len)
-      throw (bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::deleteElements(pos, len);
-    }
-  ;
-  const bpp::Alphabet* getAlphabet() const
-    {
-      return bpp::BasicSequence::getAlphabet();
-    }
-  ;
-  void setContent(const std::vector<int>& list) throw (bpp::BadIntException)
-    {
-      bpp::BasicSequence::setContent(list);
-    }
-  ;
-  const std::vector<int>& getContent() const
-    {
-      return bpp::BasicSequence::getContent();
-    }
-  ;
-  void setElement(unsigned int pos, const std::string& c)
-      throw (bpp::BadCharException, bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::setElement(pos, c);
-    }
-  ;
-  void setElement(unsigned int pos, int v) throw (bpp::BadIntException,
-      bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::setElement(pos, v);
-    }
-  ;
-  void addElement(const std::string& c) throw (bpp::BadCharException)
-    {
-      bpp::BasicSequence::addElement(c);
-    }
-  ;
+      void shuffle()
+        {
+          bpp::BasicSequence::shuffle();
+        }
+      ;
+      void deleteElement(unsigned int pos)
+          throw (bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::deleteElement(pos);
+        }
+      ;
+      void deleteElements(unsigned int pos, unsigned int len)
+          throw (bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::deleteElements(pos, len);
+        }
+      ;
+      const bpp::Alphabet* getAlphabet() const
+        {
+          return bpp::BasicSequence::getAlphabet();
+        }
+      ;
+      void setContent(const std::vector<int>& list) throw (bpp::BadIntException)
+        {
+          bpp::BasicSequence::setContent(list);
+        }
+      ;
+      const std::vector<int>& getContent() const
+        {
+          return bpp::BasicSequence::getContent();
+        }
+      ;
+      void setElement(unsigned int pos, const std::string& c)
+          throw (bpp::BadCharException, bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::setElement(pos, c);
+        }
+      ;
+      void setElement(unsigned int pos, int v) throw (bpp::BadIntException,
+          bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::setElement(pos, v);
+        }
+      ;
+      void addElement(const std::string& c) throw (bpp::BadCharException)
+        {
+          bpp::BasicSequence::addElement(c);
+        }
+      ;
 
-  void addElement(unsigned int pos, const std::string& c)
-      throw (bpp::BadCharException, bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::addElement(pos, c);
-    }
-  ;
-  void addElement(int v) throw (bpp::BadIntException)
-    {
-      bpp::BasicSequence::addElement(v);
-    }
-  ;
+      void addElement(unsigned int pos, const std::string& c)
+          throw (bpp::BadCharException, bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::addElement(pos, c);
+        }
+      ;
+      void addElement(int v) throw (bpp::BadIntException)
+        {
+          bpp::BasicSequence::addElement(v);
+        }
+      ;
 
-  void addElement(unsigned int pos, int v) throw (bpp::BadIntException,
-      bpp::IndexOutOfBoundsException)
-    {
-      bpp::BasicSequence::addElement(pos, v);
-    }
-  ;
-  unsigned int size() const
-    {
-      return bpp::BasicSequence::size();
-    }
-  ;
-  std::string toString() const
-    {
-      return bpp::BasicSequence::toString();
-    }
-  ;
-  std::string getChar(unsigned int pos) const
-      throw (bpp::IndexOutOfBoundsException)
-    {
-      return bpp::BasicSequence::getChar(pos);
-    }
-  ;
-  const int& operator[](unsigned int i) const
-    {
-      return bpp::BasicSequence::operator [](i);
-    }
-  ;
-  int& operator[](unsigned int i)
-    {
-      return bpp::BasicSequence::operator [](i);
-    }
-  ;
-  int getValue(unsigned int pos) const throw (bpp::IndexOutOfBoundsException)
-    {
-      return bpp::BasicSequence::getValue(pos);
-    }
-  ;
+      void addElement(unsigned int pos, int v) throw (bpp::BadIntException,
+          bpp::IndexOutOfBoundsException)
+        {
+          bpp::BasicSequence::addElement(pos, v);
+        }
+      ;
+      unsigned int size() const
+        {
+          return bpp::BasicSequence::size();
+        }
+      ;
+      std::string toString() const
+        {
+          return bpp::BasicSequence::toString();
+        }
+      ;
+      std::string getChar(unsigned int pos) const
+          throw (bpp::IndexOutOfBoundsException)
+        {
+          return bpp::BasicSequence::getChar(pos);
+        }
+      ;
+      const int& operator[](unsigned int i) const
+        {
+          return bpp::BasicSequence::operator [](i);
+        }
+      ;
+      int& operator[](unsigned int i)
+        {
+          return bpp::BasicSequence::operator [](i);
+        }
+      ;
+      int getValue(unsigned int pos) const
+          throw (bpp::IndexOutOfBoundsException)
+        {
+          return bpp::BasicSequence::getValue(pos);
+        }
+      ;
 
-  };
+      };
+  }
 
 #endif /* SEQUENCEPROXY_H_ */
