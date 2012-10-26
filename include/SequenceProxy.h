9f28fd2dbf7d4a6b234911f50248c4faa4b2e7a2
Daniel Exner <dex@dragonslave.de>
2012-10-26 22:48:20 +0200

    * made some Methods not virtual

--- include/SequenceProxy.h
+++ include/SequenceProxy.h
@@ -113,7 +113,7 @@ namespace Circal
           return bpp::BasicSequence::toString();
         }
       ;
-      std::string getChar(unsigned int pos) const
+      virtual std::string getChar(unsigned int pos) const
           throw (bpp::IndexOutOfBoundsException)
         {
           return bpp::BasicSequence::getChar(pos);
@@ -129,7 +129,7 @@ namespace Circal
           return bpp::BasicSequence::operator [](i);
         }
       ;
-      int getValue(unsigned int pos) const
+      virtual  int getValue(unsigned int pos) const
           throw (bpp::IndexOutOfBoundsException)
         {
           return bpp::BasicSequence::getValue(pos);
